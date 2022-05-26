dc4cbb52d05e6dfa0a2616f48ca0aef13936e16d
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-05-26 13:23:00 +0500

    fix: Task 05, fix fs/promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const {readdir} = require('fs/promises');
 const {pipeline, Transform } = require('stream');
 const path = require('path');
 const taskPath = {
@@ -7,32 +6,27 @@ const taskPath = {
   dest: path.join(__dirname, 'project-dist'),
 };
 
-(async function mergeFiles() {
+(function mergeFiles() {
   const ts = new Transform({
     transform(chunk, enc, cb) {
       this.push(`${chunk}\n`);
       cb();
     }
   });
-  await readdir(taskPath.src, {withFileTypes: true})
-    .then(files => {
-      for (let file of files) {
-        if ( file.isFile() && path.extname(file.name) === '.css' ) {
-          pipeline(
-            fs.createReadStream(path.join(taskPath.src, file.name)),
-            ts,
-            fs.createWriteStream(path.join(taskPath.dest, 'bundle.css')),
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      }      
-    })
-    .catch(err => {
-      if (err) throw err;
-    });
-    
+  fs.readdir(taskPath.src, {withFileTypes: true}, (err, files) => {
+    for (let file of files) {
+      if ( file.isFile() && path.extname(file.name) === '.css' ) {
+        pipeline(
+          fs.createReadStream(path.join(taskPath.src, file.name)),
+          ts,
+          fs.createWriteStream(path.join(taskPath.dest, 'bundle.css')),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      }
+    }      
+  });
 })();
 
 
