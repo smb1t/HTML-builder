6391b14fc660d14ea33cbba6149214a8503a824b
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-05-26 13:31:13 +0500

    fix: Task 06, fix fs/promises

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const {mkdir, copyFile, readdir, rm, readFile } = require('fs/promises');
 const path = require('path');
 const {pipeline, Transform } = require('stream');
 const taskPath = {
@@ -9,33 +8,38 @@ const taskPath = {
   dest: path.join(__dirname, 'project-dist'),
 };
 
-(async function builder() {
-  await rm(taskPath.dest, { recursive:true, force:true });
-
-  await mkdir(taskPath.dest, { recursive: true }, (err) => {
+(function builder() {
+  fs.rm(taskPath.dest, { recursive:true, force:true }, (err) => {
     if (err) throw err;
-  });
-  let template = await readFile(path.join(__dirname, 'template.html'), 'utf8', err => {
-    if (err) throw err;
-  });
-  let tags = template.match(/{{\s*([\w-]+)\s*}}/g);
-  let writer = fs.createWriteStream(path.join(taskPath.dest, 'index.html'));
 
-  for (const tag of tags) {
-    const name = tag.replace(/[{}\s]/g, '');
-    const target = path.join(taskPath.components, `${name}.html`);
-    const part = await readFile(target, 'utf8', err => {
+    fs.mkdir(taskPath.dest, { recursive: true }, (err) => {
       if (err) throw err;
+     
+      fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, data) => {
+        if (err) throw err;
+
+        let template = data;
+
+        let tags = template.match(/{{\s*([\w-]+)\s*}}/g);
+        
+        for (const tag of tags) {
+          const name = tag.replace(/[{}\s]/g, '');
+          const target = path.join(taskPath.components, `${name}.html`);
+
+          fs.readFile(target, 'utf8', (err,data) => {
+            if (err) throw err;
+            let writer = fs.createWriteStream(path.join(taskPath.dest, 'index.html'));
+            template = template.replace(tag, data);
+            writer.write(template.replace(tag, data));
+          });
+        }
+        copyAssets(taskPath.assets, path.join(taskPath.dest, 'assets'));  
+      });
     });
-    template = template.replace(tag, part);
-  }
-  writer.write(template);
-  
-  await copyAssets(taskPath.assets, path.join(taskPath.dest, 'assets'));
-  await mergeFiles();
+  });
 })();
 
-async function mergeFiles() {
+function mergeFiles() {
   const ts = new Transform({
     transform(chunk, enc, cb) {
       this.push(`${chunk}\n`);
@@ -43,46 +47,42 @@ async function mergeFiles() {
     }
   });
 
-  await readdir(taskPath.styles, {withFileTypes: true})
-    .then(files => {
-      files.push(files.splice(0, 1)[0]);
-      for (let file of files) {
-        // console.log(file);
-        if ( file.isFile() && path.extname(file.name) === '.css' ) {
-          pipeline(
-            fs.createReadStream(path.join(taskPath.styles, file.name)),
-            ts,
-            fs.createWriteStream(path.join(taskPath.dest, 'style.css')),
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      }      
-    })
-    .catch(err => {
-      if (err) throw err;
-    });
+  fs.readdir(taskPath.styles, {withFileTypes: true}, (err,files) => {
+    files.push(files.splice(0, 1)[0]);
+    for (let file of files) {
+      // console.log(file);
+      if ( file.isFile() && path.extname(file.name) === '.css' ) {
+        pipeline(
+          fs.createReadStream(path.join(taskPath.styles, file.name)),
+          ts,
+          fs.createWriteStream(path.join(taskPath.dest, 'style.css')),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      }
+    } 
+  });
 }
 
-async function copyAssets(srcPath, destPath) {
-  await mkdir(destPath, { recursive: true }, (err) => {
+function copyAssets(srcPath, destPath) {
+  fs.mkdir(destPath, { recursive: true }, (err) => {
     if (err) throw err;
-  });
-  await readdir(srcPath, {withFileTypes: true})
-    .then(files => {
+
+    fs.readdir(srcPath, {withFileTypes: true}, (err,files) => {
       for (let file of files) {
         const src = path.join(srcPath, file.name);
         const dest = path.join(destPath, file.name);
         if ( file.isFile() ) {
-          copyFile( src, dest);
+          fs.copyFile( src, dest, err => {
+            if (err) throw err;
+          });
         } else {
           copyAssets(src, dest);
         }
       }
-    })
-    .catch(err => {
-      if (err) throw err;
-    });
-}
 
+      mergeFiles();
+    });
+  });
+}
