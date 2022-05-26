bf5b8e8853a6598c84459ce88a32bfd8d23fec15
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-05-26 13:18:19 +0500

    fix: Task 03, fix fs/promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,18 @@
-const { stat } = require('fs');
-const {readdir} = require('fs/promises');
+const { stat, readdir } = require('fs');
 const path = require('path');
 
 (function getFileInfo() {
   const taskPath = `${__dirname}/secret-folder`;
-  readdir(taskPath, {withFileTypes: true})
-    .then(files => {
-      // console.log('\n');
-      for (let file of files) {
-        stat(path.join(taskPath,file.name), (err, stats) => {
-          if ( file.isFile() ) {
-            const name = file.name.split('.').slice(0, -1).join('.');
-            const ext = path.extname(file.name).split('.').pop();
-            const size = Math.round(stats.size / 1024 * 1000) / 1000 + 'Kb';
-            console.log(`${name} - ${ext} - ${size}`);
-          }
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  readdir(taskPath, {withFileTypes: true}, (err, files) => {
+    for (let file of files) {
+      stat(path.join(taskPath,file.name), (err, stats) => {
+        if ( file.isFile() ) {
+          const name = file.name.split('.').slice(0, -1).join('.');
+          const ext = path.extname(file.name).split('.').pop();
+          const size = Math.round(stats.size / 1024 * 1000) / 1000 + 'Kb';
+          console.log(`${name} - ${ext} - ${size}`);
+        }
+      });
+    }
+  });
 })();
