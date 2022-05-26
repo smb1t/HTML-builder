2bd9318e9df56b83dce57c1657e4afc23ab786a8
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-05-26 13:22:33 +0500

    fix: Task 04, fix fs/promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const {mkdir, copyFile, readdir, rm } = require('fs/promises');
+const {mkdir, copyFile, readdir, rm } = require('fs');
 const path = require('path');
 const defaultPath = path.join(__dirname, 'files');
 const taskPath = {
@@ -6,20 +6,23 @@ const taskPath = {
   dest: `${defaultPath}-copy`,
 };
 
-(async function copyFiles() {
-  await rm(taskPath.dest, { recursive:true, force:true });
-  await mkdir(taskPath.dest, { recursive: true }, err => {
+(function copyFiles() {
+  rm(taskPath.dest, { recursive:true, force:true },err => {
     if (err) throw err;
-  });
-  await readdir(taskPath.src, {withFileTypes: true})
-    .then(files => {
-      for (let file of files) {
-        const src = path.join(taskPath.src, file.name);
-        const dest = path.join(taskPath.dest, file.name);
-        copyFile(src, dest);
-      }
-    })
-    .catch(err => {
+
+    mkdir(taskPath.dest, { recursive: true }, (err) => {
       if (err) throw err;
+
+      readdir(taskPath.src, {withFileTypes: true}, (err, files) => {
+        for (let file of files) {
+          const src = path.join(taskPath.src, file.name);
+          const dest = path.join(taskPath.dest, file.name);
+          copyFile(src, dest, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+
     });
+  });
 })();
