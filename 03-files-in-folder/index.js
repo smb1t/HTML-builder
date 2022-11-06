2489807fc0129914008ca50a7b0be2f0e35ad8fb
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-11-06 10:18:02 +0500

    refactor: refactor task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,33 @@
-const { stat, readdir } = require('fs');
+const { readdir, stat } = require('fs/promises');
 const path = require('path');
+const color = '\x1b[33m%s\x1b[0m';
 
-(function getFileInfo() {
-  const taskPath = `${__dirname}/secret-folder`;
-  readdir(taskPath, {withFileTypes: true}, (err, files) => {
-    for (let file of files) {
-      stat(path.join(taskPath,file.name), (err, stats) => {
-        if ( file.isFile() ) {
+(() => {
+  const p = `${__dirname}/secret-folder`;
+
+  readdir(p, { withFileTypes: true })
+    .then(async files => {
+      for (const [index, file] of files.entries()) {
+        const stats = await stat(path.join(p, file.name));
+
+        if (index === 0) console.log(color, '\nList of files in the "secret folder" directory:\n');
+
+        if (file.isFile()) {
           const name = file.name.split('.').slice(0, -1).join('.');
           const ext = path.extname(file.name).split('.').pop();
-          const size = Math.round(stats.size / 1024 * 1000) / 1000 + 'Kb';
-          console.log(`${name} - ${ext} - ${size}`);
+          const i = parseInt(Math.floor(Math.log(stats.size) / Math.log(1024)), 10);
+
+          if (i === 0) {
+            console.log(`${name} - ${ext} - ${stats.size}Kb`);
+          } else {
+            console.log(`${name} - ${ext} - ${(stats.size / (1024 ** i)).toFixed(1)}Kb`);
+          }
         }
-      });
-    }
-  });
+      }
+    })
+    .catch(err => {
+      if (err) throw err;
+    }).finally(() => {
+      console.log(color, '\nThat\'s all!\n');
+    });
 })();
