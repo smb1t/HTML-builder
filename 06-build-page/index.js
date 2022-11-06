48f6653efb6e243e3de8dd9580e861fef21782e8
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-11-06 10:38:38 +0500

    refactor: refactor task 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,88 +1,130 @@
 const fs = require('fs');
+const fsp = require('fs/promises');
 const path = require('path');
-const {pipeline, Transform } = require('stream');
-const taskPath = {
-  assets: path.join(__dirname, 'assets'),
-  components: path.join(__dirname, 'components'),
-  styles: path.join(__dirname, 'styles'),
-  dest: path.join(__dirname, 'project-dist'),
-};
 
-(function builder() {
-  fs.rm(taskPath.dest, { recursive:true, force:true }, (err) => {
-    if (err) throw err;
+class HTMLBuilder {
+  constructor() {
+    this.htmlTpl = 'template.html';
+    this.html = 'index.html';
+    this.css = 'style.css';
+    this.path = {
+      assets: path.join(__dirname, 'assets'),
+      components: path.join(__dirname, 'components'),
+      styles: path.join(__dirname, 'styles'),
+      dest: path.join(__dirname, 'project-dist'),
+    };
+    this.path.assetsDest = path.join(this.path.dest, 'assets');
+  }
 
-    fs.mkdir(taskPath.dest, { recursive: true }, (err) => {
-      if (err) throw err;
-     
-      fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, data) => {
-        if (err) throw err;
+  async build() {
+    await this.createDist(this.path.dest);
 
-        let template = data;
+    this.copyAssets(this.path.assets, this.path.assetsDest);
+    this.mergeStyles();
+    this.buildHtml();
 
-        let tags = template.match(/{{\s*([\w-]+)\s*}}/g);
-        
+    this.printMessage('\nProject building complete!');
+  }
+
+  getSortedFiles(arr, fileName) {
+    const i = arr.findIndex(el => el.name.indexOf(fileName) >= 0);
+    const curr = arr[i];
+    arr.splice(i, 1);
+    arr.unshift(curr);
+    return fileName !== 'header' ? this.getSortedFiles(arr, 'header') : arr;
+  }
+
+  async printMessage(mes, color) {
+    const c = color ? color : '\x1b[33m%s\x1b[0m';
+    console.log(c, mes, '\n');
+  }
+
+  async createDist(dest) {
+    await fsp.access(dest)
+      .then(async () => {
+        await fsp.rm(dest, { recursive: true, force: true });
+        await fsp.mkdir(dest, { recursive: true });
+      })
+      .catch(() => {
+        fsp.mkdir(dest, { recursive: true });
+      });
+  }
+
+  async buildHtml() {
+    const components = await fsp.readdir(this.path.components, { withFileTypes: true });
+    const htmlData = new Object();
+
+    for (const file of components) {
+      if (file.isFile() && path.extname(path.join(this.path.components, file.name)) === '.html') {
+        htmlData[file.name.split('.')[0]] = await fsp.readFile(path.join(this.path.components, file.name), 'utf-8');
+      }
+    }
+
+    await fsp.readFile(path.join(__dirname, this.htmlTpl), 'utf8')
+      .then((template) => {
+        let output = template;
+        const tags = template.match(/{{\s*([\w-]+)\s*}}/g);
+
         for (const tag of tags) {
           const name = tag.replace(/[{}\s]/g, '');
-          const target = path.join(taskPath.components, `${name}.html`);
-
-          fs.readFile(target, 'utf8', (err,data) => {
-            if (err) throw err;
-            let writer = fs.createWriteStream(path.join(taskPath.dest, 'index.html'));
-            template = template.replace(tag, data);
-            writer.write(template.replace(tag, data));
-          });
+          output = output.replace(tag, htmlData[name]);
         }
-        copyAssets(taskPath.assets, path.join(taskPath.dest, 'assets'));  
+
+        const writer = fs.createWriteStream(path.join(this.path.dest, this.html));
+        writer.write(output);
+      })
+      .catch(err => {
+        if (err) throw err;
       });
-    });
-  });
-})();
+  }
 
-function mergeFiles() {
-  const ts = new Transform({
-    transform(chunk, enc, cb) {
-      this.push(`${chunk}\n`);
-      cb();
-    }
-  });
+  copyAssets(src, dest) {
+    fsp.mkdir(dest, { recursive: true });
 
-  fs.readdir(taskPath.styles, {withFileTypes: true}, (err,files) => {
-    files.push(files.splice(0, 1)[0]);
-    for (let file of files) {
-      // console.log(file);
-      if ( file.isFile() && path.extname(file.name) === '.css' ) {
-        pipeline(
-          fs.createReadStream(path.join(taskPath.styles, file.name)),
-          ts,
-          fs.createWriteStream(path.join(taskPath.dest, 'style.css')),
-          (err) => {
-            if (err) throw err;
+    fsp
+      .readdir(src, { withFileTypes: true })
+      .then(files => {
+        for (const file of files) {
+          const srcFile = path.join(src, file.name);
+          const destFile = path.join(dest, file.name);
+
+          if (file.isFile()) {
+            fsp.copyFile(srcFile, destFile)
+              .catch(err => {
+                if (err) throw err;
+              });
+          } else {
+            this.copyAssets(srcFile, destFile);
           }
-        );
-      }
-    } 
-  });
-}
-
-function copyAssets(srcPath, destPath) {
-  fs.mkdir(destPath, { recursive: true }, (err) => {
-    if (err) throw err;
-
-    fs.readdir(srcPath, {withFileTypes: true}, (err,files) => {
-      for (let file of files) {
-        const src = path.join(srcPath, file.name);
-        const dest = path.join(destPath, file.name);
-        if ( file.isFile() ) {
-          fs.copyFile( src, dest, err => {
-            if (err) throw err;
-          });
-        } else {
-          copyAssets(src, dest);
         }
-      }
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
 
-      mergeFiles();
-    });
-  });
+  async mergeStyles() {
+    const ws = fs.createWriteStream(path.join(this.path.dest, this.css), 'utf-8');
+
+    await fsp.readdir(this.path.styles, { withFileTypes: true })
+      .then(data => {
+        const files = this.getSortedFiles(data, 'footer');
+
+        for (const file of files) {
+          if (file.isFile() && path.extname(file.name) === '.css') {
+            fs
+              .createReadStream(path.join(this.path.styles, file.name), 'utf-8')
+              .on('data', chunk => {
+                ws.write(chunk);
+              });
+          }
+        }
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
 }
+
+const app = new HTMLBuilder();
+app.build();
